874ea8eaef54a9d300c5c37bc9310e0a10a1472f
kageanrage <kevinjpickett@gmail.com>
2018-08-21 17:18:14 +1000

    fixed column & row counting functions

--- SE Admin scrape.py
+++ SE Admin scrape.py
@@ -539,16 +539,19 @@ def oldDataExcelToDictImporter(excel_filename): # given excel filename, creates 
     dict = {}
     headersDict = excelHeadingsGrabber(excel_filename)
     num_of_cols = columnCounter(excel_filename)
+    num_of_rows = rowCounter(excel_filename)
+    lenOfmergedDict = len(mergedDict)
+    logging.debug(f'Should be same: {num_of_rows}, and {lenOfmergedDict}')
     for k, v in headersDict.items():
         if v == 'Project number':
             projectNumberColumn = k
             logging.debug(f'P- number is in column {projectNumberColumn}')
-    for row in range(2,len(mergedDict)): # TODO: need to calculate #rows via a function, not relying on len of mergedDict
+    for row in range(2,num_of_rows): # TODO: need to calculate #rows via a function, not relying on len of mergedDict
         nestedDict = {}
         for column in range(1,num_of_cols):
             cell = sheet.cell(row = row, column = column)
             v = cell.value
-            print(f'row is {row}. Column is {column}, value is {v}')
+            # print(f'row is {row}. Column is {column}, value is {v}')
             nestedKey = headersDict[column]
             nestedVal = v
             nestedDict.setdefault(nestedKey, nestedVal)
@@ -564,13 +567,30 @@ def columnCounter(xls_filename): #checks row 1 and counts how many cells have da
     sheet = wb.active
     cols = 1 # start from 1st column
     while 1:
-        check_cell = sheet.cell(row = 1, column = cols)
-        v = check_cell.value
-        if v != None: #if there is data in the cell
+        cell = sheet.cell(row = 1, column = cols)
+        v = cell.value
+        if v != None: # if there is data in the cell
             cols += 1 # check the next column along
         else:    # if no data in the cell, then that's the last column, so break
+            break
+    return int(cols)-1 # need to be minus one because it increments cols, then realises it's an empty cell
+
+
+def rowCounter(xls_filename): #checks column 1 and counts how many cells have data, therefore how many rows in xls
+    logging.debug('Counting rows in xls')
+    wb = openpyxl.load_workbook(xls_filename)
+    sheet = wb.active
+    rows = 1 # start from 1st column
+    while 1:
+        check_cell = sheet.cell(row = rows, column = 1)
+        v = check_cell.value
+        if v != None: #if there is data in the cell
+            rows += 1 # check the next column along
+        else:    # if no data in the cell, then that's the last row, so break
              break
-    return cols
+    return int(rows)-1 # need to be minus one because it increments rows, then realises it's an empty cell
+
+
 
 
 def excelHeadingsGrabber(xls_filename): # checks row 1 of xls and returns a dictionary showing col# & heading
@@ -590,10 +610,7 @@ def excelHeadingsGrabber(xls_filename): # checks row 1 of xls and returns a dict
     return dic
 
 
-# headers = excelHeadingsGrabber('data.xlsx')
-
-
-importedDict = oldDataExcelToDictImporter('data.xlsx')
+# importedDict = oldDataExcelToDictImporter('data.xlsx')
 
 
 # moNew = process_soup(exampleNewSoup)
